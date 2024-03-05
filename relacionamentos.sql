37ad1c13b6deda128b0f67c400696d16945a072a
muffliv <livymendes5@gmail.com>
2024-03-05 20:17:14 -0300

    Atividade 1 - Banco de Dados 3 - Maria Lívia - REV. 02

--- relacionamentos.sql
+++ relacionamentos.sql
@@ -1,7 +1,7 @@
-ALTER TABLE tbl_item_produto ADD CONSTRAINT fk_produto
+ALTER TABLE tbl_item_pedido ADD CONSTRAINT fk_produto
 FOREIGN KEY (cod_prod)
 REFERENCES tbl_produto(cod_prod);
 
-ALTER TABLE tbl_item_produto ADD CONSTRAINT fk_pedido
+ALTER TABLE tbl_item_pedido ADD CONSTRAINT fk_pedido
 FOREIGN KEY (cod_ped)
 REFERENCES tbl_pedido(cod_ped);
--- tabelas.sql
+++ tabelas.sql
@@ -9,11 +9,13 @@ preco		DECIMAL(10,2) NOT NULL
 );
 
 create table tbl_pedido(
-cod_ped 	INT(10) PRIMARY KEY,
+cod_ped 	INT(10) AUTO_INCREMENT PRIMARY KEY,
 data_ped 	VARCHAR(10)
 );
 
-create table tbl_item_produto(
+create table tbl_item_pedido(
 cod_prod	INT(10),
 cod_ped		INT(10)
 );
+
+drop database atv1_relacao_pedido;
